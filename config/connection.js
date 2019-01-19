12af84e41a475e543f6a748689a24d4f006dbb79
rafactor <avila.rafael@gmail.com>
2019-01-19 17:45:26 -0500

    backend working

--- config/connection.js
+++ config/connection.js
@@ -2,19 +2,21 @@ var mysql = require('mysql');
 
 var connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
+    port: 8889,
     user: "root",
     password: "root",
     database: "burger_db"
 })
 
-module.exports = connection.connect(function(err){
+// Make connection.
+connection.connect(function(err) {
     if (err) {
-        console.log('error connecting:' + err.stack);
-        return;
+      console.error("error connecting: " + err.stack);
+      return;
     }
-
     console.log("connected as id " + connection.threadId);
-
-})
+  });
+  
+  // Export connection for our ORM to use.
+  module.exports = connection;
 
--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 var express = require('express');
 var app = express();
-var exphbs = require('express-handlebars');
-var connection = require('./config/connection');
 
 var PORT = process.env.PORT || 8080;
 
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
 // Parse request body as JSON
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
-connection
-
+// Set Handlebars.
+var exphbs = require('express-handlebars');
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-
+// Import routes and give the server access to them.
+var routes = require("./controllers/burger_controller");
+app.use(routes);
 
 app.listen(PORT, function(){
     console.log(`app listening on the PORT: ${PORT}`)
